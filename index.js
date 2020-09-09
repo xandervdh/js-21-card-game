245ddcacfc4c8391e5d37d9d4e53d1f570c0ca27
xandervdh <xander-v.d.h@hotmail.com>
2020-09-09 16:48:18 +0200

    made the cards appear on the screen when you ask one

--- index.js
+++ index.js
@@ -7,6 +7,9 @@
     let newCard;
     let suits = ["clubs", "diamonds", "hearts", "spades"];
     let ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+    let cardImage;
+    let cardNumberPlayer;
+    let cardNumberDealer;
 
     let startButton = document.getElementById("start");
 
@@ -31,7 +34,7 @@
         }
         for (let x = 0; x < 52; x++) {
             let numbers = x + 1
-            let image = numbers + ".png";
+            let image = "images/" + numbers + ".png";
             deck[x].image = image;
         }
     }
@@ -46,34 +49,53 @@
         }
     }
 
-    /*function randomCard() {
-        random = Math.floor(Math.random() * (13 - 1)) + 1;
-        if (random == 11 || random == 12 || random == 13){
-            random = 10;
-        } else if (random == 1){
-            random = 11;
-        }
-    }*/
-
     function topCard() {
         newCard = deck.pop();
         console.log(newCard);
     }
 
+    function setCardsPlayer() {
+        cardImage[cardNumberPlayer].setAttribute("src", newCard.image);
+        cardNumberPlayer++;
+    }
+
+    function setCardsDealer() {
+        cardImage[cardNumberDealer].setAttribute("src", newCard.image);
+        cardNumberDealer++;
+    }
+
+    function cardReset() {
+        for (let x = 0; x < cardImage.length; x++){
+            cardImage[x].setAttribute("src", "images/red_back.png")
+        }
+    }
+
     function start() {
+        cardImage = document.getElementsByClassName("cards");
         user = 0;
         random = 0;
         card = 0;
         deck = [];
+        cardNumberPlayer = 5;
+        cardNumberDealer = 0;
+        cardReset();
         createDeck();
         shuffle(deck);
         console.log(deck);
         topCard();
+        setCardsPlayer();
         user += newCard.value;
         topCard();
+        setCardsDealer()
+        card += newCard.value;
+        topCard();
+        setCardsPlayer();
+        topCard();
+        setCardsDealer()
+        card += newCard.value;
+        console.log("start pc " + card);
         user += newCard.value;
         console.log("start user " + user);
-        alert("the total of your 2 starting cards is " + user);
         askCard();
     }
 
@@ -81,6 +103,7 @@
     function dealerCard() {
         if (card < 15) {
             topCard();
+            setCardsDealer();
             card += newCard.value;
             console.log("computer " + card);
             dealerCard();
@@ -92,9 +115,9 @@
         let question = window.confirm("Do you want to draw another card?");
         if (question == true) {
             topCard();
+            setCardsPlayer();
             user += newCard.value;
             console.log("user " + user);
-            alert("your card is " + newCard.rank + " of " + newCard.suit +  " \nyour current total is " + user);
             if (user > 21) {
                 alert("you are busted, GAME OVER!!");
             } else {
@@ -104,11 +127,6 @@
             alert("you are busted, GAME OVER!!");
         } else {
 
-            topCard();
-            card += newCard.value;
-            topCard();
-            card += newCard.value;
-            console.log("start pc " + card);
             dealerCard();
             winner();
         }
