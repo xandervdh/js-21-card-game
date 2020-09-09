3a957ae7ecaea02b34b54e8229f446d93d0c9c69
xandervdh <xander-v.d.h@hotmail.com>
2020-09-09 10:46:28 +0200

    finished it with simple rules

---
+++ index.js
@@ -0,0 +1,73 @@
+(() => {
+
+    let user = 0;
+    let random = 0;
+    let card = 0;
+    start();
+
+    function randomCard() {
+        random = Math.floor(Math.random() * (13 - 1)) + 1;
+        //console.log(random);
+    }
+
+    function start() {
+        randomCard();
+        user += random;
+        console.log("user start " + random);
+        alert("your current card is " + random);
+        askCard();
+    }
+
+
+    function dealerCard() {
+        if (card < 15) {
+            randomCard();
+            console.log("new card " + random)
+            card += random;
+            console.log("computer " + card);
+            dealerCard();
+        }
+
+    }
+
+    function askCard() {
+        let question = window.prompt("Do you want to draw another card?")
+        if (question == "yes") {
+            randomCard();
+            user += random;
+            console.log("user " + user);
+            alert("your card is " + random + " your current total is " + user);
+            if (user > 21) {
+                alert("you are busted, GAME OVER!!");
+                playAgain();
+            } else {
+                askCard();
+            }
+        } else {
+            randomCard();
+            card += random;
+            console.log("computer start " + card);
+            dealerCard();
+            winner();
+        }
+    }
+
+    function winner() {
+        if (card < user && card <= 21) {
+            alert("you won!!");
+            playAgain();
+        } else if (card > user && card <= 21) {
+            alert("you lost");
+            playAgain();
+        }
+    }
+
+    function playAgain() {
+        let again = prompt("want to play again?");
+        if (again == "yes"){
+            start();
+        }
+    }
+
+
+})();
