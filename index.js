89793276ec09cbbe45f491b2095783ab62d40e29
xandervdh <xander-v.d.h@hotmail.com>
2020-09-10 15:11:32 +0200

    added some comments in the code to explain it

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 (() => {
-
+    //all the variables initiated and getting some elements out of the html
     let user = 0;
-        let card = 0;
+    let card = 0;
     let deck = [];
     let newCard;
     let suits = ["clubs", "diamonds", "hearts", "spades"];
@@ -17,6 +17,7 @@
     let hitButton = document.getElementById("hit");
     let standButton = document.getElementById("stand");
 
+    //event listener for the start button
     startButton.addEventListener("click", function () {
         hitButton.disabled = false;
         standButton.disabled = false;
@@ -24,6 +25,45 @@
         start();
     })
 
+    //event listener for the hit button
+    hitButton.addEventListener('click', function () {
+        setCardsPlayer();
+        console.log("user " + user);
+        if (user > 21) {
+            hitButton.disabled = true;
+            standButton.disabled = true;
+            startButton.disabled = false;
+            resetAnimation();
+            setTimeout(function (){
+                cardImage[1].setAttribute("src", dSecondCard.image);
+                cardImage[1].classList.toggle("flip");
+            }, 100)
+            card += dSecondCard.value;
+            addDealerCounter()
+            setTimeout(function (){
+                alert("you are busted, GAME OVER!!");
+            },200);
+        }
+    })
+
+    //event listener for stand button
+    standButton.addEventListener("click", function (){
+        hitButton.disabled = true;
+        standButton.disabled = true;
+        startButton.disabled = false;
+        cardImage[1].classList.remove("flip");
+        setTimeout(function (){
+            cardImage[1].setAttribute("src", dSecondCard.image);
+            cardImage[1].classList.toggle("flip");
+        }, 100)
+        card += dSecondCard.value;
+        addDealerCounter()
+        cardNumberDealer = 2;
+        setTimeout(function (){dealerCard()}, 1000);
+        setTimeout(function (){winner()}, 2000);
+    })
+
+    //creating the deck of cards
     function createDeck() {
         for (let i = 0; i < suits.length; i++) {
             for (let j = 0; j < ranks.length; j++) {
@@ -46,6 +86,7 @@
         }
     }
 
+    //shuffle the deck
     function shuffle(a) {
         let j, x, i;
         for (i = a.length - 1; i > 0; i--) {
@@ -56,11 +97,13 @@
         }
     }
 
+    //put the top card into a variable
     function topCard() {
         newCard = deck.pop();
         console.log(newCard);
     }
 
+    //put the player's cards on the playing field and update the counter
     function setCardsPlayer() {
         topCard();
         cardImage[cardNumberPlayer].setAttribute("src", newCard.image);
@@ -70,6 +113,7 @@
         cardNumberPlayer++;
     }
 
+    //put the dealer's cards on the playing field and update the counter
     function setCardsDealer() {
         topCard();
         cardImage[cardNumberDealer].setAttribute("src", newCard.image);
@@ -79,6 +123,7 @@
         cardNumberDealer++;
     }
 
+    //save the dealer's blind card
     function secondCardDealer() {
         topCard();
         cardImage[1].setAttribute("src", "images/red_back.png");
@@ -86,20 +131,24 @@
         dSecondCard = newCard;
     }
 
+    //reset the playing field to the black borders
     function cardReset() {
         for (let x = 0; x < cardImage.length; x++) {
             cardImage[x].setAttribute("src", "images/black_border.png")
         }
     }
 
+    //update the dealer's counter
     function addDealerCounter() {
         dCounter.innerHTML = card;
     }
 
+    //update the player's counter
     function addPlayerCounter() {
         pCounter.innerHTML = user;
     }
 
+    //start new game
     function start() {
         cardImage = document.getElementsByClassName("cards");
         user = 0;
@@ -121,7 +170,7 @@
         console.log("start user " + user);
     }
 
-
+    //let the dealer decide to take another card or not
     function dealerCard() {
         if (card < 15) {
             setCardsDealer();
@@ -130,42 +179,14 @@
         }
     }
 
+    //reset the flip animation
     function resetAnimation() {
         for (let x = 0; x < cardImage.length; x++) {
             cardImage[x].classList.remove("flip");
         }
     }
 
-    hitButton.addEventListener('click', function () {
-        setCardsPlayer();
-        console.log("user " + user);
-        if (user > 21) {
-            hitButton.disabled = true;
-            standButton.disabled = true;
-            startButton.disabled = false;
-            resetAnimation();
-            alert("you are busted, GAME OVER!!");
-        }
-    })
-
-    standButton.addEventListener("click", function (){
-        hitButton.disabled = true;
-        standButton.disabled = true;
-        startButton.disabled = false;
-        cardImage[1].classList.remove("flip");
-        setTimeout(function (){
-            cardImage[1].setAttribute("src", dSecondCard.image);
-            cardImage[1].classList.toggle("flip");
-        }, 100)
-        card += dSecondCard.value;
-        addDealerCounter()
-        cardNumberDealer = 2;
-        setTimeout(function (){dealerCard()}, 1000);
-        setTimeout(function (){winner()}, 2000);
-        setTimeout(function (){resetAnimation()}, 2000);
-        //resetAnimation();
-    })
-
+    //announce if you lost or won
     function winner() {
         if (card < user && card <= 21) {
             alert("you won!!");
